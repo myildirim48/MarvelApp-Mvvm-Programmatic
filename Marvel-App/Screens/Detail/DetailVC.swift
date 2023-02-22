31812b0eeddef7d27d60a818bdab61b258411ed0
Muhammed YILDIRIM <muhammedyildirim48@gmail.com>
2023-02-22 12:52:01 +0300

    Set DetailVC with ScrollView and prepare for ViewModel

--- Marvel-App/Screens/Detail/DetailVC.swift
+++ Marvel-App/Screens/Detail/DetailVC.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 
-class DetailVC: LoadingVC {
+class DetailVC: UICollectionViewController {
 
+    let scrollView = UIScrollView()
+    let contentView = UIView()
+    
     let imageView = ImageView(frame: .zero)
     let nameLabel = MrLabel(textAligment: .left, font: Theme.fonts.titleFont)
     let descriptionLabel = MrLabel(textAligment: .left, font: Theme.fonts.desriptionFont)
@@ -23,6 +26,8 @@ class DetailVC: LoadingVC {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
+        configureScrollView()
+        configureCollectionView()
         setupUI()
         
     }
@@ -45,18 +50,39 @@ class DetailVC: LoadingVC {
         imageView.downloadImage(fromUrl: charachter.thumbnail.path)
         
     }
+
+    private func configureScrollView(){
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        
+        scrollView.pinToEdges(of: view)
+        contentView.pinToEdges(of: scrollView)
+        
+        NSLayoutConstraint.activate([
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            contentView.heightAnchor.constraint(equalToConstant: 1500)
+        ])
+    }
+    
+    private func configureCollectionView(){
+        
+        collectionView.delegate = self
+        collectionView.setCollectionViewLayout(UICollectionViewLayoutGenerator.resourcesCollectionViewLayout(), animated: false)
+        collectionView.register(ResourceCell.self, forCellWithReuseIdentifier: ResourceCell.reuseIdentifier)
+        collectionView.register(TitleSupplementaryView.self, forSupplementaryViewOfKind: TitleSupplementaryView.elementKind, withReuseIdentifier: TitleSupplementaryView.reuseIdentifier)
+    }
+    
     
     private func configureViewController() {
         view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
-        UINavigationBar.appearance().backgroundColor = .systemRed
     }
     
     
     private func setupUI() {
         
-        view.addSubviews(imageView,nameLabel,descriptionLabel,favoritedButton)
+        contentView.addSubviews(imageView,nameLabel,descriptionLabel,favoritedButton,collectionView)
         view.backgroundColor = .systemBackground
         
         favoritedButton.addTarget(self, action: #selector(self.favoriteButtonTapped(_:)), for: .touchUpInside)
@@ -65,25 +91,30 @@ class DetailVC: LoadingVC {
         let imgHeight : CGFloat = view.frame.height / 2.5
         
         
-        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            imageView.topAnchor.constraint(equalTo: view.topAnchor,constant: spacing*2),
-            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor,constant: spacing*2),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
             imageView.heightAnchor.constraint(equalToConstant: imgHeight),
             
             nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor,constant: 2*spacing),
-            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: spacing),
-            nameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -spacing),
+            nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor,constant: spacing),
+            nameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor,constant: -spacing),
             
             favoritedButton.topAnchor.constraint(equalTo: imageView.bottomAnchor,constant: spacing),
-            favoritedButton.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -spacing),
+            favoritedButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor,constant: -spacing),
             favoritedButton.widthAnchor.constraint(equalToConstant: 40),
             favoritedButton.heightAnchor.constraint(equalToConstant: 37),
             
             descriptionLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor,constant: 2*spacing),
             descriptionLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
-            descriptionLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor)
+            descriptionLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor),
+            
+            collectionView.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor,constant: spacing),
+            collectionView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
             
         ])
     }
--- Marvel-App/Utilities/UICollectionViewLayoutGenerator.swift
+++ Marvel-App/Utilities/UICollectionViewLayoutGenerator.swift
@@ -86,4 +86,69 @@ struct UICollectionViewLayoutGenerator {
             return section
         }
     }
+    //MARK: - Resources layout
+    
+    enum ResourceSection: Int, CaseIterable {
+        case comics, stories, events, series
+        
+        var sectionTitle: String {
+            switch self {
+                case .comics: return "Comics"
+                case .stories: return "Stories"
+                case .events: return "Events"
+                case .series: return "Series"
+            }
+        }
+        
+        func columnCount(for width: CGFloat) -> Int {
+            let wideMode = width > 800
+            let narrowMode = width < 420
+            
+            switch self {
+                case .comics, .stories, .events, .series:
+                    return wideMode ? 3 : narrowMode ? 1 : 2
+            }
+        }
+        
+        var groupSize: (width: NSCollectionLayoutDimension, height: NSCollectionLayoutDimension) {
+            switch self {
+                case .comics: return (width: .absolute(138), height: .absolute(221))
+                case .events: return (width: .fractionalWidth(0.40), height: .fractionalHeight(0.18))
+                case .series: return (width: .fractionalWidth(0.40), height: .fractionalHeight(0.18))
+                case .stories: return (width: .fractionalWidth(0.40), height: .fractionalHeight(0.18))
+            }
+        }
+    }
+    
+    static func resourcesCollectionViewLayout() -> UICollectionViewLayout {
+        
+        let sectionProvider = { (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            let sectionLayoutKind = ResourceSection(rawValue: sectionIndex)!
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+            let sectionGroupSize = sectionLayoutKind.groupSize
+            let groupSize = NSCollectionLayoutSize(widthDimension: sectionGroupSize.width, heightDimension: sectionGroupSize.height)
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.orthogonalScrollingBehavior = .continuous
+            section.interGroupSpacing = 20
+            section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20)
+            
+            let titleSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(44))
+            let titleSupplementary = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: titleSize, elementKind: TitleSupplementaryView.elementKind, alignment: .top)
+            section.boundarySupplementaryItems = [titleSupplementary]
+        
+            return section
+        }
+        
+        let config = UICollectionViewCompositionalLayoutConfiguration()
+        config.interSectionSpacing = 20
+        config.scrollDirection = .vertical
+        let layout = UICollectionViewCompositionalLayout(sectionProvider: sectionProvider, configuration: config)
+        
+        return layout
+    }
 }
